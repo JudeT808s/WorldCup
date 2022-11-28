161bc6f97f5b1cd64bb86520a0630339db21055e
jude <n00213344@iadt.ie>
2022-11-28 16:02:55 +0000

    New branch

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,12 @@ Route::get('/dashboard', function () {
 require __DIR__ . '/auth.php';
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+
 Route::get('/home/tournament', [App\Http\Controllers\HomeController::class, 'tournamentIndex'])->name('home.tournament.index');
 
 
 // This will create all the routes for Book
 // and the routes will only be available when a user is logged in
-Route::resource('/admin/books', TournamentController::class)->middleware(['auth'])->names('admin.books');
+Route::resource('/admin/tournament', TournamentController::class)->middleware(['auth'])->names('admin.tournament');
 
-Route::resource('/user/books', TournamentController::class)->middleware(['auth'])->names('user.books')->only(['index', 'show']);
+Route::resource('/user/tournament', TournamentController::class)->middleware(['auth'])->names('user.tournament')->only(['index', 'show']);
